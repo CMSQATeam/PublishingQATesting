71987b73396c46f48e6e05dfc35cbac62413c38a
CMSQATeam <cmsqa123@gmail.com>
2019-03-06 15:29:36 +0530

    DW CMS Message

--- ScriptRef/DiffBuilder/Model/ClassProperty.cs
+++ ScriptRef/DiffBuilder/Model/ClassProperty.cs
@@ -1,14 +1,23 @@
 namespace DiffPlex.DiffBuilder.Model
 {
     
+    /// <summary>
+    /// Structure describing a permission that requires user authorization.
+    /// </summary>
+    /// <description>
+    /// Used when requesting permission or checking if permission has been granted to use the camera.<br/> <br/> <br/> <br/> <br/> <br/>  [5c7f6e20eed1782094fe94e3](delegatecode_5c7f6e20eed1782094fe94e3.cs)
+    /// </description>
     public class ClassProperty
     {
+        
         
         public int NumberA { get; set; }
 
         
+        
         public int NumberB { get; set; }
 
+        
         
         public int NumberC { get; set; }
     }
