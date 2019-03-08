a2e9a54dcb01509b20896460dcca02f7b982322e
CMSQATeam <cmsqa123@gmail.com>
2019-03-08 16:07:20 +0530

    DW CMS Message

--- ScriptRef/DiffBuilder/Model/ClassProperty.cs
+++ ScriptRef/DiffBuilder/Model/ClassProperty.cs
@@ -1,14 +1,20 @@
 namespace DiffPlex.DiffBuilder.Model
 {
     
+    /// <summary>
+    /// Controls if the advertisement system should be initialized immediately on startup
+    /// </summary>
     public class ClassProperty
     {
+        
         
         public int NumberA { get; set; }
 
         
+        
         public int NumberB { get; set; }
 
+        
         
         public int NumberC { get; set; }
     }
