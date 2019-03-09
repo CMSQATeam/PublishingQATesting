f528d3a0f05111bd18b2b99a3add9358c7788ac3
CMSQATeam <cmsqa123@gmail.com>
2019-03-09 16:37:20 +0530

    DW CMS Message

--- ScriptRef/DiffBuilder/Model/ClassProperty.cs
+++ ScriptRef/DiffBuilder/Model/ClassProperty.cs
@@ -1,14 +1,33 @@
 namespace DiffPlex.DiffBuilder.Model
 {
     
+    /// <summary>
+    /// Learn to create the systems used to develop
+    /// </summary>
+    /// <description>
+    /// ![abc](angeler_5c78de84bb171e0bd0de07cd.jpg)
+    /// </description>
     public class ClassProperty
     {
         
+        /// <summary>
+        /// An adventure game in this intermediate
+        /// </summary>
+        /// <description>
+        /// [5c7674e2bf9f0a0194bc7676](2D01_5c7674e2bf9f0a0194bc7676.cs)
+        /// </description>
         public int NumberA { get; set; }
 
         
+        /// <summary>
+        /// Presented in 6 phases
+        /// </summary>
+        /// <description>
+        /// []()
+        /// </description>
         public int NumberB { get; set; }
 
+        
         
         public int NumberC { get; set; }
     }
