e2695f32b66a0e6f5d0fa0dd165f14846f74769b
CMSQATeam <cmsqa123@gmail.com>
2019-03-14 18:36:55 +0530

    DW CMS Message

--- ScriptRef/IMerger.cs
+++ ScriptRef/IMerger.cs
@@ -9,12 +9,25 @@ using System.Text;
 namespace DiffPlex
 {
     
+    /// <summary>
+    /// codeblock5
+    /// </summary>
+    /// <description>
+    /// [5c8a4ba40c6ec2070cd6d895](codeblock5_5c8a4ba40c6ec2070cd6d895.cs)
+    /// </description>
     public interface IMerger
     {
+        
         
         SideBySideDiffModel GetMergedText(List<string> list);
 
         
+        /// <summary>
+        /// codeblock6
+        /// </summary>
+        /// <description>
+        /// [5c8a4bbc0c6ec2070cd6d8d8](codeblock6_5c8a4bbc0c6ec2070cd6d8d8.cs)
+        /// </description>
         SideBySideDiffModel GetMergedText(SideBySideDiffModel model);
     }
 }
