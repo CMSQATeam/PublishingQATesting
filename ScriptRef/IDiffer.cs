313237f7e8a7996065e7eab68b5a544c9fda7a61
CMSQATeam <cmsqa123@gmail.com>
2019-03-21 10:30:58 +0530

    DW CMS Message

--- ScriptRef/IDiffer.cs
+++ ScriptRef/IDiffer.cs
@@ -4,42 +4,49 @@ using DiffPlex.Model;
 namespace DiffPlex
 {
     /// <summary>
-                /// Provides methods for generate differences between texts
-                /// </summary>
-                /// <description>
-                /// Testing publishing in scriptref
-                /// </description>
-                            public interface IDiffer
+                                /// Provides methods for generate differences between texts
+                                /// </summary>
+                                /// <description>
+                                /// Testing publishing in scriptref
+                                /// </description>
+                                                            public interface IDiffer
     {
+        
         
         
         DiffResult CreateLineDiffs(string oldText, string newText, bool ignoreWhitespace);
         
         
+        
         DiffResult CreateLineDiffs(string oldText, string newText, bool ignoreWhitespace, bool ignoreCase);
         
         /// <summary>
-        /// ![The Project View displays all source files and created prefabs](Editor-Project_5c74fe3fa66a08177c1c0ade.png)
-        /// </summary>
-        /// <returns>
-        /// [5c75240dbf9f0a0194bc1071](code_2_5c75240dbf9f0a0194bc1071.cs)
-        /// </returns>
-        /// <param name="newText">
-        /// []()
-        /// </param>
-        DiffResult CreateCharacterDiffs(string oldText, string newText, bool ignoreWhitespace);
+                /// ![The Project View displays all source files and created prefabs](Editor-Project_5c74fe3fa66a08177c1c0ade.png)
+                /// </summary>
+                /// <returns>
+                /// [5c75240dbf9f0a0194bc1071](code_2_5c75240dbf9f0a0194bc1071.cs)
+                /// </returns>
+                /// <param name="newText">
+                /// []()
+                /// </param>
+                        DiffResult CreateCharacterDiffs(string oldText, string newText, bool ignoreWhitespace);
+        
         
         
         DiffResult CreateCharacterDiffs(string oldText, string newText, bool ignoreWhitespace, bool ignoreCase);
         
         
+        
         DiffResult CreateWordDiffs(string oldText, string newText, bool ignoreWhitespace, char[] separators);
+        
         
         
         DiffResult CreateWordDiffs(string oldText, string newText, bool ignoreWhitespace, bool ignoreCase, char[] separators);
         
         
+        
         DiffResult CreateCustomDiffs(string oldText, string newText, bool ignoreWhiteSpace, Func<string, string[]> chunker);
+        
         
         
         DiffResult CreateCustomDiffs(string oldText, string newText, bool ignoreWhiteSpace, bool ignoreCase, Func<string, string[]> chunker);
